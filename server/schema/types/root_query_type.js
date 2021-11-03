4fc7bf6b68df411423b73fb4ae0e65162c42bf37
Adolf <owner@Users-MacBook-Air.local>
2021-11-03 16:40:16 -0700

    add logic to check authentication status

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,23 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLID } = graphql;
+const UserType = require('./user_type');
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyField: { type: GraphQLID },
+    // We need to have a filed here when we are first creating our queries/mutations (it can't be empty)
+    // so we can just add in a dummy field for now, until we add something in later:
+    // dummyField: { type: GraphQLID },
+
+    // Logic to check authentication status.
+    // So, if a user is logged in, we will know that the user is logged in (authenticated).
+    // If a user is not logged in and we try to query the user, we get 'null' because the user is not authenticated.
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      },
+    },
   },
 });
 
