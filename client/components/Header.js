c6d9a078e408e06664c5e16f0df2c277f91193b7
Adolf <owner@Users-MacBook-Air.local>
2021-11-05 11:10:57 -0700

    add CurrentUser query

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class Header extends Component {
   render() {
+    // Log the result of our query that we imported
+    // to see if the user is authenticated or not:
+    console.log(this.props.data);
     return <div>Header</div>;
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
