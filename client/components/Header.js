9980dfa3146fabdcc7df46002b54b52ba0ebb518
Adolf <britvaica@netscape.net>
2021-11-05 11:42:19 -0700

    add CurrentUser query

--- client/components/Header.js
+++ client/components/Header.js
@@ -5,7 +5,7 @@ import query from '../queries/CurrentUser';
 class Header extends Component {
   render() {
     // Log the result of our query that we imported
-    // to see if the user is authenticated or not.
+    // to see if the user is authenticated or not:
     console.log(this.props.data);
     return <div>Header</div>;
   }
