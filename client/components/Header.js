42dcc749009665c75f3cb8fa9cf4778e2563e2c2
Adolf <britvaica@netscape.net>
2021-11-06 18:03:13 -0700

    add authentication state to header

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,11 +3,34 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
+  renderButtons() {
+    // Destructure:
+    const { loading, user } = this.props.data;
+    // If query is not yet complete, do not show any buttons yet (we don't know yet if user is authenticated or not,
+    // so we don't know yet which set of buttons to show) -
+    // so, if query is still loading, return an empty div:
+    if (loading) {
+      return <div />;
+    }
+
+    // If a user exists - if there is a currently authenticated user, show a Logout button
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      // If a user does not exist - if there is not a currently authenticated user, show a Login button
+      return <div>You're not signed in.</div>;
+    }
+  }
+
   render() {
     // Log the result of our query that we imported
     // to see if the user is authenticated or not:
-    console.log(this.props.data);
-    return <div>Header</div>;
+    // console.log(this.props.data);
+    return (
+      <nav>
+        <div className='nav-wrapper'>{this.renderButtons()}</div>
+      </nav>
+    );
   }
 }
 
