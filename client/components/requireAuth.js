d5eee247ac9b70a1557afb20b985c87178cde537
Adolf <britvaica@netscape.net>
2021-11-07 18:38:08 -0800

    fix RequireAuth component

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -11,6 +11,10 @@ import CurrentUser from '../queries/CurrentUser';
 // If our application had other pages that are visible only to users who are authenticated, e.g., Settings or Profile page,
 // then we would also use this HOC in those components (i.e., Settings or Profile components).
 
+// Note: 'componentDidMount' is only called when a component is first rendered on the screen.
+// Note: 'componentWillUpdate' is called every single time that the query updates its state in any fashion.
+// Any time that our query updates in any way, shape, or form, 'componentWillUpdate' will be called.
+
 export default (WrappedComponent) => {
   class RequireAuth extends Component {
     // Whenever this component has been rendered,
@@ -19,14 +23,18 @@ export default (WrappedComponent) => {
     // if that user exists, that means the user must be signed in.
     // If that property does not exist, that means the user is NOT signed in (authenticated),
     // and we need to redirect the user somewhere else in our application.
-    componentDidMount() {
+
+    // componentDidMount() {
+
+    componentWillUpdate(nextProps) {
       // If the user does not exist yet
       // Also if the query is still loading, we don't know if the user exists yet,
       // and so we wouldn't want to prematurely redirect the user over to the Login page,
       // so we also need to include: !this.props.data.loading =>
       // so if it's not loading anymore (if we're done loading the query), and there is no user present,
       // then redirect the user to the Login page:
-      if (!this.props.data.loading && !this.props.data.user) {
+      // if (!this.props.data.loading && !this.props.data.user) {
+      if (!nextProps.data.loading && !nextProps.data.user) {
         // then redirect the user back to the Login page
         hashHistory.push('/login');
       }
