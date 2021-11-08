9a2c21478c280a08cd8da315d4d6f9ed362f9182
Adolf <britvaica@netscape.net>
2021-11-07 17:57:25 -0800

    add route for Dashboard

--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
+import Dashboard from './components/Dashboard';
 
 // Note on cookies ('createNetworkInterface'):
 // Whereas the GraphiQL tool attaches cookies to a request, GraphQL does NOT attach cookies to a request,
@@ -50,6 +51,7 @@ const Root = () => {
         <Route path='/' component={App}>
           <Route path='/login' component={LoginForm}></Route>
           <Route path='/signup' component={SignupForm}></Route>
+          <Route path='/dashboard' component={Dashboard}></Route>
         </Route>
       </Router>
     </ApolloProvider>
